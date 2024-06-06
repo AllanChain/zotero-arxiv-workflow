a910171a50bed782662b92210751548d3faa951e
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2024-06-06 19:36:25 +0800

    fix: avoid using arXiv DOI from semantic scholar

--- src/modules/arxiv-update.ts
+++ src/modules/arxiv-update.ts
@@ -109,7 +109,8 @@ export class arXivUpdate {
     try {
       const jsonResp = await fetch(semanticURL);
       const semanticJSON = (await jsonResp.json()) as any;
-      return semanticJSON.externalIds?.DOI;
+      const doi = semanticJSON.externalIds?.DOI as string | undefined;
+      return doi?.toLowerCase()?.includes("arxiv") ? undefined : doi
     } catch (err) {
       ztoolkit.log(err);
     }
