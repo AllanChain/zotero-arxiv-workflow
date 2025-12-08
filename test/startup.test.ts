3f77848c26533e1d89c9d80681a0b85b05c460a7
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2025-12-08 08:39:40 +0800

    fix: add test about public API expose

--- test/startup.test.ts
+++ test/startup.test.ts
@@ -5,4 +5,12 @@ describe("startup", function () {
   it("should have plugin instance defined", function () {
     assert.isNotEmpty(Zotero[config.addonInstance]);
   });
+
+  it("should expose public API", function () {
+    const plugin = Zotero[config.addonInstance];
+    assert.isDefined(plugin.api.arXivUpdate);
+    assert.isDefined(plugin.api.merge);
+    assert.isDefined(plugin.api.preferPDF);
+    assert.isDefined(plugin.api.updatePDF);
+  });
 });
