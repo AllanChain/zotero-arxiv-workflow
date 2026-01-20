b9018d4b6a98036c48679891efc321ae75ce4f03
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2026-01-20 21:54:27 +0800

    test: check if preference is registered

--- test/startup.test.ts
+++ test/startup.test.ts
@@ -2,8 +2,14 @@ import { assert } from "chai";
 import { config } from "../package.json";
 
 describe("startup", function () {
-  it("should have plugin instance defined", function () {
+  it("should have plugin instance and prefs defined", function () {
     assert.isNotEmpty(Zotero[config.addonInstance]);
+    assert.isDefined(
+      Zotero.Prefs.get(
+        "extensions.zotero.arxiv-workflow.merge.reservedKeys",
+        true,
+      ),
+    );
   });
 
   it("should expose public API", function () {
